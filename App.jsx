0336225699f53ec919d897cc52e4925c77678d5e
Utkarsh <utkarshsinghal369@gmail.com>
2024-03-20 15:22:18 +0530

    Integate the touch id

--- App.jsx
+++ App.jsx
@@ -1,17 +1,50 @@
-import React from 'react';
+/* eslint-disable prettier/prettier */
+import React, {useEffect, useState} from 'react';
 import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import AllBooks from './src/screen/AllBooks';
 import Login from './src/screen/Login';
 import AddBooks from './src/screen/AddBooks';
-
 import SignupScreen from './src/screen/Signup';
 import AdminBooks from './src/screen/AdminBooks';
 import UpdateBook from './src/screen/UpdateBook';
+import TouchID from 'react-native-touch-id'; // Assuming TouchID is properly installed
 
 function App() {
+  const [isAuth, setIsAuth] = useState(false);
   const Stack = createNativeStackNavigator();
+
+  const optionalConfigObject = {
+    unifiedErrors: false,
+    passcodeFallback: false,
+  };
+
+  const handleBiometric = () => {
+    TouchID.isSupported(optionalConfigObject)
+      .then(biometryType => {
+        // Success code
+        if (biometryType === 'FaceID') {
+          console.log('FaceID is supported.');
+          setIsAuth(true);
+        } else {
+          console.log('TouchID is supported.');
+          TouchID.authenticate('', optionalConfigObject).then(success => {
+            setIsAuth(true);
+            console.log('success', success);
+          });
+          setIsAuth(true);
+        }
+      })
+      .catch(error => {
+        // Failure code
+        console.log(error);
+        setIsAuth(false);
+      });
+  };
+  useEffect(() => {
+    handleBiometric();
+  }, []);
 
   return (
     <NavigationContainer>
